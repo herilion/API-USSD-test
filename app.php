caa9f4d570df21e13423ebfe6c2f87996799cd2a
herilion <heritierlionge@yahoo.com>
2023-06-03 14:39:38 +0200

    return to 1

--- app.php
+++ app.php
@@ -62,3 +62,9 @@ else if ($text == "1*3") {
     $response = "CON You are about to book a table for 6 \n";
     $response .= "Please Enter 1 to confirm \n";
 }
+// Menu for a user who enters "1" from the fifth menu
+else if ($text == "1*3*1") {
+    $response = "CON Table for 6 cost -N- 250,000.00 \n";
+    $response .= "Enter 1 to continue \n";
+    $response .= "Enter 0 to cancel";
+}
