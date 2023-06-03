b3c86067a023465f308a4221898ae62216af0ebd
herilion <heritierlionge@yahoo.com>
2023-06-03 14:14:02 +0200

    add first menu screen

---
+++ app.php
@@ -0,0 +1,7 @@
+<?php// Reads the variables sent via POST
+$sessionId   = $_POST["sessionId"];  
+$serviceCode = $_POST["serviceCode"];  
+$text = $_POST["text"];
+//This is the first menu screen
+if ( $text == "" ) {$response  = "CON Hi welcome, I can help you with Event Reservation \n";
+    $response .= "1. Enter 1 to continue";}
